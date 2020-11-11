b480f447c002530e4fc72c458740978c866e27f3
Nem Nesic <nemnesic@gmail.com>
2020-11-11 16:44:48 -0500

    better description

--- open-jira-links-in-new-window.user.js
+++ open-jira-links-in-new-window.user.js
@@ -2,7 +2,7 @@
 // @name        open jira links in new window
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  open jira links in new window
+// @description  This little user script will append "Open in new window" to every link in your jira tickets. CONFSERVER-11194 - https://jira.atlassian.com/browse/CONFSERVER-11194 
 // @author       nemnesic@gmail.com
 // @match        ADD_YOUR_JIRA_URL_HERE/*
 // @require      https://code.jquery.com/jquery-3.5.1.js
